f467b2c356084228622d3c73527432a4a4d6b014
hache0099 <hache0099_github@protonmail.com>
2024-02-16 16:51:42 -0300

    Editado el modelo para que los campos coincidan con la DB y se añadió la relación correspondiente

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,6 +6,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
@@ -31,7 +32,7 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $hidden = [
-        'password',
+        'pass',
         //~ 'remember_token',
     ];
 
@@ -44,4 +45,8 @@ class User extends Authenticatable
         //~ 'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+    
+    function persona() {
+		return $this->belongsTo(Persona::class, "rela_persona");
+	}
 }
