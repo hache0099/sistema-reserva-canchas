eb45bd37d2450d17f097e0cdc0a83d393286e3c3
hache0099 <hache0099_github@protonmail.com>
2024-02-16 16:52:54 -0300

    Creada la view para ver el perfil de un usuario

---
+++ resources/views/user/profile.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+	<body>
+		<h1>Hola</h1>
+		<p>DNI: {{ $persona->DNI }}</p>
+		<p>Nombre: {{ $persona->Nombre }}</p>
+		<p>Apellido: {{ $persona->Apellido }}</p>
+		<p>Correo: {{ $user->email }}</p>
+		<p>Telefono: {{ $persona->Telefono }}</p>
+	</body>
+	
+</html>
