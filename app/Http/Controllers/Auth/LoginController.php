ce083f989eb9a1e626bd0ca216828613a260beb3
hache0099 <hache0099_github@protonmail.com>
2024-03-06 01:55:36 -0300

    Cambiado funciones para el login

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+
 
 class LoginController extends Controller
 {
@@ -15,15 +17,18 @@ class LoginController extends Controller
     
     function login(Request $request)
     {
-		$this->validate($request, [
+		$credentials = $request->validate([
         'email' => 'required|string|email|max:255',
         'password' => 'required|string|min:8',
 		]);
 
-		if (Auth::attempt(['email' => $request->email, 'password' => $request->password])) {
+		//['e-mail' => $request->email, 'pass' => $request->password]
+		
+		if (Auth::attempt($credentials)) {
 			return redirect()->intended(route('home'));
 		}
+		//~ $user = User::where('email')
 
-		return back()->withErrors(['email' => 'Las credenciales no coinciden.']);
+		return back()->withErrors(['email' => 'Las credenciales no coinciden.'])->onlyInput('email');
 	}
 }
