b492cc865a209a66eada9d92ad9632744c6323d8
hache0099 <hache0099_github@protonmail.com>
2024-03-06 01:52:43 -0300

    Añadido el apellido del usuario

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,7 +4,7 @@
 		<title>home</title>
 	</head>
 	<body>
-		<>
-		Logeado correctamente!!!
+		<h1>Logeado correctamente!!!</h1>
+		<h2>Bienvenido {{ Auth::user()->persona()->first()->Nombre }} {{ Auth::user()->persona()->first()->Apellido }}!!</h2>
 	</body>
 </html>
