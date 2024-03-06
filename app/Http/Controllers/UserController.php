33b677f63e8a1b8f469e1853810397def40e28c7
hache0099 <hache0099_github@protonmail.com>
2024-03-06 01:54:36 -0300

    Añadida función `store`

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 
 class UserController extends Controller
@@ -30,6 +31,10 @@ class UserController extends Controller
     public function store(Request $request)
     {
         //
+        $user = new User;
+        $user->email = $request->email;
+        $user->password = Hash::make($request->password);
+        
     }
 
     /**
