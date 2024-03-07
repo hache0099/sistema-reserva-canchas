cd147e13a2dd8de274e71cd016fc8d71f6953ec1
hache0099 <hache0099_github@protonmail.com>
2024-03-07 01:13:58 -0300

    Cambiado y actualizado la vista de login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,31 +1,33 @@
-<!DOCTYPE html>
-<html lang="es">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login</title>
-</head>
-<body>
-    <h1>Login</h1>
-    @if ($errors->any())
-        <ul>
-            @foreach ($errors->all() as $error)
-                <li>{{ $error }}</li>
-            @endforeach
-        </ul>
-    @endif
-    <form method="POST" action="/validateLogin">
-        @csrf
-
-        <label for="email">Correo electrónico</label>
-        <input type="email" id="email" name="email" required>
-
-        <label for="password">Contraseña</label>
-        <input type="password" id="password" name="password" required>
-
-        <button type="submit">Iniciar sesión</button>
-    </form>
-
-    
-</body>
-</html>
+@extends('layout.loginlayout')
+@section('content')
+<!--
+<div class='d-flex align-items-center py-4 bg-body-tertiary'>
+-->
+	<div class='m-auto mw-25'>
+		<h1 class='h3 mb-3'>Login</h1>
+		@if ($errors->any())
+			<div class='alert alert-danger'>
+			<ul>
+				@foreach ($errors->all() as $error)
+					<li>{{ $error }}</li>
+				@endforeach
+			</ul>
+			</div>
+		@endif
+		<form method="POST" action="/validateLogin">
+			@csrf
+			<div class='form-floating'>
+				<input class=form-control placeholder='nombre@ejemplo.com' type="email" id="email" name="email" required>
+				<label class=form-label for="email">Correo electrónico</label>
+			</div>
+			<div class='form-floating'>
+				<input class=form-control placeholder=password type="password" id="password" name="password" required>
+				<label class=form-label for="password">Contraseña</label>
+			</div>
+			<button class="btn btn-primary w-100 py-3" type="submit">Iniciar sesión</button>
+		</form>
+	</div>
+<!--
+</div>
+-->
+@endsection
