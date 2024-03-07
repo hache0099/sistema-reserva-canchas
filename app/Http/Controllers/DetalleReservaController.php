83a6ca0bb13387a69832209e725a98c568d35462
hache0099 <hache0099_github@protonmail.com>
2024-03-07 16:15:59 -0300

    Creado el controlador para `DetalleReserva`

---
+++ app/Http/Controllers/DetalleReservaController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreDetalleReservaRequest;
+use App\Http\Requests\UpdateDetalleReservaRequest;
+use App\Models\DetalleReserva;
+
+class DetalleReservaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreDetalleReservaRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(DetalleReserva $detalleReserva)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(DetalleReserva $detalleReserva)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateDetalleReservaRequest $request, DetalleReserva $detalleReserva)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(DetalleReserva $detalleReserva)
+    {
+        //
+    }
+}
