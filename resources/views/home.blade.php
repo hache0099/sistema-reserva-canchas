2864546f89473939de419cfd13e5f2a4d42cdf18
hache0099 <hache0099_github@protonmail.com>
2024-03-07 01:13:12 -0300

    Arreglado acceso a la información del usuario logeado

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,6 +5,9 @@
 	</head>
 	<body>
 		<h1>Logeado correctamente!!!</h1>
-		<h2>Bienvenido {{ Auth::user()->persona()->first()->Nombre }} {{ Auth::user()->persona()->first()->Apellido }}!!</h2>
+		<h2>Bienvenido {{ Auth::user()->persona->Nombre }} {{ Auth::user()->persona->Apellido }}!!</h2>
+		
+		<a href='/reservas'>Ir a mis reservas</a>
+		<a href='/logout'>Cerrar sesión</a>
 	</body>
 </html>
