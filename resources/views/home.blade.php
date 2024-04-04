558372f03ffd3351762f31dbac8f3a581e039f76
hache0099 <hache0099_github@protonmail.com>
2024-04-04 20:00:12 -0300

    Editado layout

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,13 +1,8 @@
-<!DOCTYPE HTML>
-<html>
-	<head>
-		<title>home</title>
-	</head>
-	<body>
+@extends('layout.mainlayout')
+@section('content')
 		<h1>Logeado correctamente!!!</h1>
 		<h2>Bienvenido {{ Auth::user()->persona->Nombre }} {{ Auth::user()->persona->Apellido }}!!</h2>
 		
 		<a href='/reservas'>Ir a mis reservas</a>
 		<a href='/logout'>Cerrar sesión</a>
-	</body>
-</html>
+@endsection
