b14cf3c30a79eaabe5a294451152e56b84f983c0
hache0099 <hache0099_github@protonmail.com>
2024-04-04 19:59:22 -0300

    Añadida info de la tabla

--- app/Models/TipoCancha.php
+++ app/Models/TipoCancha.php
@@ -4,8 +4,17 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class TipoCancha extends Model
 {
     use HasFactory;
+    protected $table = 'TipoCancha';
+    protected $primaryKey = 'idTipoCancha';
+    public $timestamps = false;
+
+    function cancha() : HasOne
+    {
+        return $this->hasOne(Cancha::class, 'rela_cancha');
+    }
 }
