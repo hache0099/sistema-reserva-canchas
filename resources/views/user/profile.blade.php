44bf74bb3fe2850a15b35860f5300ed28a13a84a
hache0099 <hache0099_github@protonmail.com>
2024-04-04 19:59:59 -0300

    Editado layout

--- resources/views/user/profile.blade.php
+++ resources/views/user/profile.blade.php
@@ -1,12 +1,11 @@
-<!DOCTYPE html>
-<html>
-	<body>
-		<h1>Hola</h1>
-		<p>DNI: {{ $persona->DNI }}</p>
-		<p>Nombre: {{ $persona->Nombre }}</p>
-		<p>Apellido: {{ $persona->Apellido }}</p>
-		<p>Correo: {{ $user->email }}</p>
-		<p>Telefono: {{ $persona->Telefono }}</p>
-	</body>
-	
-</html>
+@extends('layout.mainlayout')
+@section('content')
+<div class=container>
+	<h1>Hola</h1>
+	<p>DNI: {{ $persona->DNI }}</p>
+	<p>Nombre: {{ $persona->Nombre }}</p>
+	<p>Apellido: {{ $persona->Apellido }}</p>
+	<p>Correo: {{ $user->email }}</p>
+	<p>Telefono: {{ $persona->Telefono }}</p>
+</div>
+@endsection
