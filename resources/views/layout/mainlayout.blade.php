5c57a713c439cdc047647854f86274437e73fb45
hache0099 <hache0099_github@protonmail.com>
2024-04-04 20:00:23 -0300

    Editado navbar

--- resources/views/layout/mainlayout.blade.php
+++ resources/views/layout/mainlayout.blade.php
@@ -4,8 +4,10 @@
 		@include('layout.partials.head')
 	</head>
 	<body>
-		@include('layout.partials.nav')
+		@include('layout.partials.navbar')
+<!--
 		@include('layout.partials.header')
+-->
 		@yield('content')
 		@include('layout.partials.footer')
 		@include('layout.partials.footer-scripts')
