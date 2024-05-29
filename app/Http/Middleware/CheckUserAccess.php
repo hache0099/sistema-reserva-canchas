c9a28c386fdc1dca153a6614751b95ecfe67e855
hache0099 <hache0099_github@protonmail.com>
2024-05-28 23:08:45 -0300

    Añadida la función para que el middleware maneje los perfiles

--- app/Http/Middleware/CheckUserAccess.php
+++ app/Http/Middleware/CheckUserAccess.php
@@ -6,6 +6,7 @@ use Closure;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Illuminate\Support\Facades\Auth;
+use App\Models\Modulo;
 
 class CheckUserAccess
 {
@@ -14,13 +15,23 @@ class CheckUserAccess
      *
      * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
      */
-    public function handle(Request $request, Closure $next, $perfil): Response
+    public function handle(Request $request, Closure $next, $route): Response
     {
         $user = Auth::user();
 
         $user_perfil = $user->perfil;
 
-        
+        // $hasAccess = $user_perfil->perfilmodulo->modulo->where('Modelo_ruta', $route)->exists();
+
+        $hasAccess = Modulo::where('ruta', $route)
+        ->whereHas('perfiles', function ($query) use ($user_perfil) {
+            $query->where('Perfil_descripcion', $user_perfil->Perfil_descripcion);
+        })
+        ->exists();
+
+        if(!$hasAccess){
+            return response('No autorizado',403);
+        }
 
         return $next($request);
     }
