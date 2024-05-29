15bc55c019681a19946bea401f9b507124812458
hache0099 <hache0099_github@protonmail.com>
2024-05-29 16:13:18 -0300

    Añadidos los nuevos modelos para el registro

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -5,6 +5,11 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Models\User;
 use App\Models\Persona;
+use App\Models\TipoDocumento;
+use App\Models\TipoContacto;
+use App\Models\PersonaContacto;
+use App\Models\PersonaDocumento;
+use App\Models\Domicilio;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
@@ -14,7 +19,12 @@ class RegisterController extends Controller
     //
     function show()
     {
-		return view('auth.register');
+		$tiposDocumento = TipoDocumento::all();
+		$tiposContacto = TipoContacto::all();
+		return view('auth.register', [
+			// 'tiposContacto' => $tiposContacto,
+			'tiposDocumento' => $tiposDocumento
+		]);
 	}
 	
 	function validateRegister(Request $request){
@@ -25,7 +35,10 @@ class RegisterController extends Controller
 			'name' => 'required|string|max:45',
 			'apellido' => 'required|string|max:45',
 			'telefono' => 'required|numeric',
+			'tipodni' => 'required|numeric',
 			'dni' => 'required|numeric',
+			'domicilio' => 'required|max:100',
+			'fechanac' => 'required|date',
 		]);
 		
 		if ($request->password != $request->password_confirmation)
@@ -33,19 +46,31 @@ class RegisterController extends Controller
 			return back()->withErrors(['password_confirmation' => 'Las contraseñas no coinciden.']);
 		}
 		
-		$user = User::create([
-			'email' => $request->email,
-			'pass' => $request->password
-		]);
 		
 		$persona_id = Persona::create([
 			'Nombre' => $request->name,
 			'Apellido' => $request->apellido,
 			'Telefono' => $request->telefono,
-			'DNI' => $request->dni,
-			'rela_usuario' => $user->id_usuario
+			'FechaNacimiento' => $request->fechanac,
+		]);
+
+		$domicilio = Domicilio::create([
+			'Domicilio_detalle' => $request->domicilio,
+			'rela_persona' => $persona_id,
+		]);
+
+		$persona_doc = PersonaDocumento::create([
+			'PesonaDocumento_desc' => $request->dni,
+			'Persona_idPersona' => $persona_id,
+			'TipoDocumento_idTipoDocumento' => $request->tipodni,
 		]);
 		
+		$user = User::create([
+			'email' => $request->email,
+			'pass' => $request->password,
+			'rela_persona' => $persona_id
+		]);
+
 		Auth::login($user);
 		$request->session()->regenerate();
 		return redirect()->intended(route('home'));
