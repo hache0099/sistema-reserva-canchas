d7ce163b610afa409ff8187b953305abe70f0a74
hache0099 <hache0099_github@protonmail.com>
2024-05-31 21:57:22 -0300

    Cambiado el formato de casteo de `fecha_alta`

--- app/Models/User.php
+++ app/Models/User.php
@@ -48,7 +48,7 @@ class User extends Authenticatable
      * @var array<string, string>
      */
     protected $casts = [
-        'fecha_alta' => 'date',
+        'fecha_alta' => 'datetime:Y-m-d',
         'password' => 'hashed',
     ];
     
