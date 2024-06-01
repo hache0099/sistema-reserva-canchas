7014688995bb1ad1a6e3d3b0b05c59cc70676b8a
hache0099 <hache0099_github@protonmail.com>
2024-05-31 21:20:57 -0300

    Añadida transacción para crear el usuario de forma segura

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Models\User;
 use App\Models\Persona;
+use App\Models\Perfil;
 use App\Models\TipoDocumento;
 use App\Models\TipoContacto;
 use App\Models\PersonaContacto;
@@ -13,6 +14,8 @@ use App\Models\Domicilio;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\DB;
+use Throwable;
 
 class RegisterController extends Controller
 {
@@ -28,14 +31,14 @@ class RegisterController extends Controller
 	}
 	
 	function validateRegister(Request $request){
-		$this->validate($request,[
+		$valido = $request->validate([
 			'email' => 'required|string|email|max:55',
 			'password' => 'required|string|min:8',
 			'password_confirmation' => 'required|string|min:8',
 			'name' => 'required|string|max:45',
 			'apellido' => 'required|string|max:45',
 			'telefono' => 'required|numeric',
-			'tipodni' => 'required|numeric',
+			'tipodni' => 'required|numeric|min:1',
 			'dni' => 'required|numeric',
 			'domicilio' => 'required|max:100',
 			'fechanac' => 'required|date',
@@ -45,31 +48,50 @@ class RegisterController extends Controller
 		{
 			return back()->withErrors(['password_confirmation' => 'Las contraseñas no coinciden.']);
 		}
-		
-		
-		$persona_id = Persona::create([
-			'Nombre' => $request->name,
-			'Apellido' => $request->apellido,
-			'Telefono' => $request->telefono,
-			'FechaNacimiento' => $request->fechanac,
-		]);
 
-		$domicilio = Domicilio::create([
-			'Domicilio_detalle' => $request->domicilio,
-			'rela_persona' => $persona_id,
-		]);
-
-		$persona_doc = PersonaDocumento::create([
-			'PesonaDocumento_desc' => $request->dni,
-			'Persona_idPersona' => $persona_id,
-			'TipoDocumento_idTipoDocumento' => $request->tipodni,
-		]);
+		if ($request->tipodni < 1){
+			return back()->withErrors(['tipodni' => 'Escoja un tipo de DNI']);
+		}
 		
-		$user = User::create([
-			'email' => $request->email,
-			'pass' => $request->password,
-			'rela_persona' => $persona_id
-		]);
+		try {
+			DB::beginTransaction();
+			$persona = Persona::create([
+				'Nombre' => $request->name,
+				'Apellido' => $request->apellido,
+				//'Telefono' => $request->telefono,
+				'FechaNacimiento' => $request->fechanac,
+			]);
+
+			$domicilio = Domicilio::create([
+				'Domicilio_detalle' => $request->domicilio,
+				'rela_persona' => $persona->id_persona,
+			]);
+
+			$persona_doc = PersonaDocumento::create([
+				'PesonaDocumento_desc' => $request->dni,
+				'Persona_id_persona' => $persona->id_persona,
+				'TipoDocumento_id_TipoDocumento' => $request->tipodni,
+			]);
+
+			$persona_contacto = PersonaContacto::create([
+				'PersonaContacto_desc' => $request->telefono,
+				'rela_persona' => $persona->id_persona,
+				'rela_tipocontacto' => TipoContacto::where('Contacto_descripcion', 'Telefono')->first()->idContacto,
+			]);
+			
+			$user = User::create([
+				'email' => $request->email,
+				'password' => $request->password,
+				'rela_persona' => $persona->id_persona,
+				'rela_perfil' => Perfil::where('Perfil_descripcion', 'Usuario')->value('idPerfil'),
+				'fecha_alta' => date('Ymd')
+			]);
+			DB::commit();
+		} catch (Throwable $exception) {
+			// TODO: redirigir a página de error
+			DB::rollBack();
+			return back()->withErrors(['error_registro' => 'Hubo un error al registrarte: ' . $exception])->withInput();
+		}
 
 		Auth::login($user);
 		$request->session()->regenerate();
