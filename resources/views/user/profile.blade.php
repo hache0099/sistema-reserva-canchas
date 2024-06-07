10beb1d4ea2393e826bdbca7554c85ffb99a43a8
hache0099 <hache0099_github@protonmail.com>
2024-06-06 21:35:00 -0300

    Añadidos más campos de perfil

--- resources/views/user/profile.blade.php
+++ resources/views/user/profile.blade.php
@@ -2,11 +2,13 @@
 @section('content')
 <div class=container>
 	<h1>Hola</h1>
-	<p>DNI: {{ $dni }}</p>
 	<p>Nombre: {{ $persona->Nombre }}</p>
 	<p>Apellido: {{ $persona->Apellido }}</p>
+	<p>Tipo DNI: {{ $tipodni }}</p>
+	<p>DNI: {{ $dni }}</p>
+	
 	<p>Correo: {{ $user->email }}</p>
-	<p>Telefono: {{ $telefono->PersonaContacto_desc }}</p>
+	<p>Telefono: {{ $telefono }}</p>
 	<p>Domicilio: {{ $domicilio }}
 </div>
 @endsection
