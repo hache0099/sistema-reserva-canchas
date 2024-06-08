4b7811eff8b141a29ffc0e62f7aa1f82100638d0
hache0099 <hache0099_github@protonmail.com>
2024-06-07 22:03:41 -0300

    Cambiado título

--- resources/views/user/profile.blade.php
+++ resources/views/user/profile.blade.php
@@ -1,7 +1,7 @@
 @extends('layout.mainlayout')
 @section('content')
 <div class=container>
-	<h1>Hola</h1>
+	<h1>Mi Perfil</h1>
 	@if (session('status'))
 		<div class="alert alert-success" role="alert">
 			{{ session('status') }}
