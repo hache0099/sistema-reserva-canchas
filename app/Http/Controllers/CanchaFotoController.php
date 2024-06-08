924c018dc4c54c95da7ef89776d500357d08a609
hache0099 <hache0099_github@protonmail.com>
2024-06-08 01:46:25 -0300

    Creado controlador para CanchaFoto

---
+++ app/Http/Controllers/CanchaFotoController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\CanchaFoto;
+use Illuminate\Http\Request;
+
+class CanchaFotoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(CanchaFoto $canchaFoto)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(CanchaFoto $canchaFoto)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, CanchaFoto $canchaFoto)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(CanchaFoto $canchaFoto)
+    {
+        //
+    }
+}
