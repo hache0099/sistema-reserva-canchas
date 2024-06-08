da7d21a604bf843657cac8e9fca5a58ff35ef5be
hache0099 <hache0099_github@protonmail.com>
2024-06-07 21:08:38 -0300

    Añadido status de confirmación de cambios

--- resources/views/user/profile.blade.php
+++ resources/views/user/profile.blade.php
@@ -2,13 +2,21 @@
 @section('content')
 <div class=container>
 	<h1>Hola</h1>
+	@if (session('status'))
+		<div class="alert alert-success" role="alert">
+			{{ session('status') }}
+		</div>
+	@endif
 	<p>Nombre: {{ $persona->Nombre }}</p>
 	<p>Apellido: {{ $persona->Apellido }}</p>
+	<p>Fecha Nacimiento: {{ date_format($persona->FechaNacimiento, "Y-m-d") }}</p>
 	<p>Tipo DNI: {{ $tipodni }}</p>
 	<p>DNI: {{ $dni }}</p>
 	
 	<p>Correo: {{ $user->email }}</p>
 	<p>Telefono: {{ $telefono }}</p>
-	<p>Domicilio: {{ $domicilio }}
+	<p>Domicilio: {{ $domicilio }}</p>
+
+	<a href='/profile/editar' role='button' class='btn btn-primary'>Editar Perfil</a>
 </div>
 @endsection
