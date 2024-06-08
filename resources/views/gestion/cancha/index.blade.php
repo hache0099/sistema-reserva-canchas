82d0be4f8e6e58d818d15ca028c9ce3beaf0f899
hache0099 <hache0099_github@protonmail.com>
2024-06-08 02:55:35 -0300

    Creado el index que muestra las canchas disponibles

--- resources/views/gestion/cancha/index.blade.php
+++ resources/views/gestion/cancha/index.blade.php
@@ -1,4 +1,33 @@
-<div>
-    <!-- Nothing worth having comes easy. - Theodore Roosevelt -->
-    :V
+@extends('layout.mainlayout')
+@section('content')
+<div class="container my-4">
+    <h1>Gestión de Canchas</h1>
+    <table class="table table-bordered mt-4">
+        <thead>
+            <tr>
+                <th>ID Cancha</th>
+                <th>Tipo de Cancha</th>
+                <th>Cantidad Máxima de Personas</th>
+                <th>Precio por Hora</th>
+                
+                <th>Acciones</th>
+            </tr>
+        </thead>
+            @foreach($canchas as $cancha)
+            <tr>
+                
+                <td>{{ $cancha->id_cancha }}</td>
+                <td>{{ $cancha->tipocancha->Material }}</td>
+                <td>{{ $cancha->Cancha_cantidad_max_personas }}</td>
+                <td>${{ $cancha->Cancha_precio_hora }}</td>
+                
+                <td>
+                    <a href="/gestion/canchas/{{ $cancha->id_cancha }}/editar" class="btn btn-warning btn-sm">Editar</a>
+                </td>
+            </tr>
+            @endforeach
+        </tbody>
+    </table>
+    <a href="/gestion/canchas/create" class="btn btn-primary">Crear Nueva Cancha</a>
 </div>
+@endsection
