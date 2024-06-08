bf30966655c032cca145b66ca293c6e8bf5beace
hache0099 <hache0099_github@protonmail.com>
2024-06-08 01:47:30 -0300

    Añadidas funciones al controlador

--- app/Http/Controllers/TipoCanchaController.php
+++ app/Http/Controllers/TipoCanchaController.php
@@ -2,9 +2,27 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\TipoCancha;
 use Illuminate\Http\Request;
 
 class TipoCanchaController extends Controller
 {
     //
+
+    function index()
+    {
+        $tiposCancha = TipoCancha::all();
+
+        return view('gestion.tipocancha.index', ['tiposCancha' => $tiposCancha]);
+    }
+
+    function create()
+    {
+        return view('gestion.tipocancha.create');
+    }
+
+    function store(Request $request)
+    {
+        //TODO
+    }
 }
