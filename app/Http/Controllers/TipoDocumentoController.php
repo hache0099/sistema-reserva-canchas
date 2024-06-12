2d66584d0e6c451e54c06fec2926732b31bf715f
hache0099 <hache0099_github@protonmail.com>
2024-06-11 23:56:19 -0300

    Añadidas funciones para mostrar, crear y modificar datos

--- app/Http/Controllers/TipoDocumentoController.php
+++ app/Http/Controllers/TipoDocumentoController.php
@@ -3,8 +3,59 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use App\Models\TipoDocumento;
+use Throwable;
 
 class TipoDocumentoController extends Controller
 {
     //
+    function index()
+    {
+        $tiposDocumento = TipoDocumento::all();
+        return view("gestion.tipodoc.index",['tiposDocumento' => $tiposDocumento]);
+    }
+
+    function edit(int $id_tipodoc)
+    {
+        $tipoDoc = TipoDocumento::find($id_tipodoc);
+
+        return view('gestion.tipodoc.edit',['tipoDocumento' => $tipoDoc]);
+    }
+
+    function update(Request $request, $id_tipodoc)
+    {
+        $request->validate([
+            'TipoDocumento_desc' => 'required|string',
+        ]);
+
+        try
+        {
+            DB::beginTransaction();
+            $tipoDoc = TipoDocumento::find($id_tipodoc);
+
+            $tipoDoc->TipoDocumento_desc = $request['TipoDocumento_desc'];
+            $tipoDoc->save();
+            DB::commit();
+
+            return redirect('/gestion/tipos-documento')->with('status','La actualización ha sido exitosa');
+        } catch (Throwable $e)
+        {
+            DB::rollBack();
+        }
+    }
+
+    function create()
+    {
+        return view('gestion.tipodoc.create');
+    }
+
+    function store(Request $request)
+    {
+        $request->validate([
+            'TipoDocumento_desc' => 'required|string',
+        ]);
+
+        
+    }
 }
