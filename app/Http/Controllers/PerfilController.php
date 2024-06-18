9977c42435feee71754c6eb9f19cc604acb5a2aa
hache0099 <hache0099_github@protonmail.com>
2024-06-18 20:08:58 -0300

    Creadas las funciones para mostrar y editar los perfiles

--- app/Http/Controllers/PerfilController.php
+++ app/Http/Controllers/PerfilController.php
@@ -1,10 +1,24 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Models\Perfil;
 use Illuminate\Http\Request;
 
 class PerfilController extends Controller
 {
     //
+
+    function show()
+    {
+        $perfiles = Perfil::all();
+
+        return view('gestion.perfil.index',['perfiles' => $perfiles]);
+    }
+
+    function edit($id_perfil)
+    {
+        $perfil = Perfil::find($id_perfil);
+
+        return view('gestion.perfil.edit',['perfil' => $perfil]);
+    }
 }
