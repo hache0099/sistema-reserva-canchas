4241bda0df5c87242b9d53bcfd98d693733e020f
hache0099 <hache0099_github@protonmail.com>
2024-06-18 20:09:36 -0300

    Creadas las funciones para las ABM de TipoCancha

--- app/Http/Controllers/TipoCanchaController.php
+++ app/Http/Controllers/TipoCanchaController.php
@@ -16,6 +16,26 @@ class TipoCanchaController extends Controller
         return view('gestion.tipocancha.index', ['tiposCancha' => $tiposCancha]);
     }
 
+    function update(Request $request, $id_tipocancha)
+    {
+        $request->validate([
+            'Material' => 'required|string',
+        ]);
+
+        $tipoCancha = TipoCancha::find($id_tipocancha);
+        $tipoCancha->update($request->all());
+
+        return redirect('/gestion/tipos-cancha')->with('status', 'Se ha actualizado con éxito');
+
+    }
+
+    function edit($id_tipocancha)
+    {
+        $tipoCancha = TipoCancha::find($id_tipocancha);
+
+        return view('gestion.tipocancha.edit',['tipocancha' => $tipoCancha]);
+    }
+
     function create()
     {
         return view('gestion.tipocancha.create');
@@ -23,6 +43,29 @@ class TipoCanchaController extends Controller
 
     function store(Request $request)
     {
-        //TODO
+        $request->validate([
+            'Material' => 'required|string',
+        ]);
+
+        TipoCancha::create($request->all());
+
+        return redirect('/gestion/tipos-cancha')->with('status', 'Se ha creado con éxito');
+    }
+
+    function delete($id_tipocancha)
+    {
+        $tipoCancha = TipoCancha::find($id_tipocancha);
+        $tipoCancha->estado = 0;
+        $tipoCancha->save();
+
+        return redirect('/gestion/tipos-cancha')->with('status', 'Se ha borrado con éxito');
+    }
+
+    function restore($id_tipocancha)
+    {
+        $tipoCancha = TipoCancha::find($id_tipocancha);
+        $tipoCancha->estado = 1;
+        $tipoCancha->save();
+        return redirect('/gestion/tipos-cancha')->with('status', 'Se ha restaurado con éxito');
     }
 }
