58483fa9126cc14a8cb102305be289098d47f099
hache0099 <hache0099_github@protonmail.com>
2024-06-18 20:05:57 -0300

    Creados las funciones para crear, mostrar y actualizar las fotos

--- app/Http/Controllers/CanchaFotoController.php
+++ app/Http/Controllers/CanchaFotoController.php
@@ -3,7 +3,10 @@
 namespace App\Http\Controllers;
 
 use App\Models\CanchaFoto;
+use App\Models\Cancha;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
+use Throwable;
 
 class CanchaFotoController extends Controller
 {
@@ -13,22 +16,42 @@ class CanchaFotoController extends Controller
     public function index()
     {
         //
+        $canchas = Cancha::with('canchafoto')->get();
+        return view('gestion.fotoscancha.index', compact('canchas'));
+
     }
 
     /**
      * Show the form for creating a new resource.
      */
-    public function create()
+    public function create($id_cancha)
     {
         //
+        return view('gestion.fotoscancha.create', compact('id_cancha'));
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(Request $request, $canchaId)
     {
-        //
+        $request->validate([
+            'CanchaFoto_ruta' => 'required|image|max:2048',
+        ]);
+
+        $path = $request->file('CanchaFoto_ruta')->store('img/canchas','public');
+        
+        if (! $path)
+        {
+            return (back()->withErrors('No se pudo subir la imagen'));
+        }
+
+        $foto = new CanchaFoto();
+        $foto->CanchaFoto_ruta = $path;
+        $foto->Cancha_id_cancha = $canchaId;
+        $foto->save();
+
+        return redirect('/gestion/fotos-canchas')->with('success', 'Foto agregada exitosamente.');
     }
 
     /**
