703a2046d4c318b17bc86503efd20e9bb295a651
hache0099 <hache0099_github@protonmail.com>
2024-07-12 20:26:21 -0300

    Cambiado el link del inicio

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,6 +3,6 @@
 		<h1>Logeado correctamente!!!</h1>
 		<h2>Bienvenido {{ Auth::user()->persona->Nombre }} {{ Auth::user()->persona->Apellido }}!!</h2>
 		
-		<a href='/reservas'>Ir a mis reservas</a>
+		<a href='/profile'>Ir a mi perfil</a><br>
 		<a href='/logout'>Cerrar sesión</a>
 @endsection
