0c54ad9403320df52931b39accd15bb09dff75ea
hache0099 <hache0099_github@protonmail.com>
2024-07-12 20:29:31 -0300

    Añadida columna Estado

--- resources/views/gestion/tipodoc/index.blade.php
+++ resources/views/gestion/tipodoc/index.blade.php
@@ -12,6 +12,7 @@
             <tr>
                 <th>ID</th>
                 <th>Descripción del Tipo de Documento</th>
+                <th>Estado</th>
                 <th>Acciones</th>
             </tr>
         </thead>
@@ -20,11 +21,12 @@
             <tr>
                 <td>{{ $tipo->id_TipoDocumento }}</td>
                 <td>{{ $tipo->TipoDocumento_desc }}</td>
+                <td>{{ $tipo->estado == 1 ? 'Habilitado' : 'Deshabilitado'}}</td>
                 <td>
                     <a href="/gestion/tipos-documento/{{ $tipo->id_TipoDocumento }}/editar" class="btn btn-warning btn-sm">Editar</a>
                      <button 
                         class="btn btn-{{ $tipo->estado == 1 ? "danger" : "success" }} btn-sm"
-                        onclick="confirmChange('tipos-cancha',{{ $tipo->id_TipoDocumento  }},{{ $tipo->estado }});">
+                        onclick="confirmChange('tipos-documento',{{ $tipo->id_TipoDocumento  }},{{ $tipo->estado }});">
                         {{ $tipo->estado == 1 ? "Eliminar" : "Restaurar" }}
                     </button>
                 </td>
@@ -36,12 +38,9 @@
     <a href="/gestion/tipos-documento/create" class="btn btn-primary">Añadir Nuevo Tipo de Documento</a>
 </div>
 <script>
-    function confirmChange(elemento, id, toDelete) {
-        let str = toDelete == 1 ? "borrar" : "restaurar";
-        let finUrl = toDelete == 1 ? "delete" : "restore";
-        if (confirm('¿Estás seguro de que deseas ' + str +' este elemento?')) {
-            window.location.href = `/gestion/${elemento}/${id}/${finUrl}`;
-        }
-    }
+    
 </script>
+@endsection
+@section('custom-scripts')
+    <script src="{{asset('js/toggleElemento.js')}}" > </script>
 @endsection
