e6292d07660812abcb6c6438b8cfb306502e0c9e
hache0099 <hache0099_github@protonmail.com>
2024-08-20 20:25:04 -0300

    Añadida opción para mostrar los módulos disponibles para cada perfil

--- app/Http/Controllers/PerfilController.php
+++ app/Http/Controllers/PerfilController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 use App\Models\Perfil;
+use App\Models\Modulo;
+use App\Models\PerfilModulo;
 use Illuminate\Http\Request;
 
 class PerfilController extends Controller
@@ -11,8 +13,18 @@ class PerfilController extends Controller
     function show()
     {
         $perfiles = Perfil::all();
+        $perfilmodulo = PerfilModulo::all();
+        //$modulo = Modulo::all();
 
-        return view('gestion.perfil.index',['perfiles' => $perfiles]);
+        $perfilesConModulos = array();
+
+        foreach($perfiles as $p)
+        {
+            $ids = $p->perfilmodulo->pluck('Modulo_idModulo')->toArray();
+            $perfilesConModulos[$p] = Modulo::whereIn('idModulo',$ids);
+        }
+
+        return view('gestion.perfil.index',['perfiles' => $perfilesConModulos]);
     }
 
     function edit($id_perfil)
