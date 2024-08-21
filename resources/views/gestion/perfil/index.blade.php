5dfbfbad1824f9cb96c7d5410708af37bfed6add
hache0099 <hache0099_github@protonmail.com>
2024-08-20 21:54:28 -0300

    Actualizado el view para que muestre los modulos disponibles

--- resources/views/gestion/perfil/index.blade.php
+++ resources/views/gestion/perfil/index.blade.php
@@ -2,25 +2,28 @@
 @section('content')
 <div class="container my-4">
     <h1>Lista de Perfiles</h1>
-    <table class="table table-bordered">
-        <thead>
-            <tr>
-                <th>ID Perfil</th>
-                <th>Descripción</th>
-                <th>Acciones</th>
-            </tr>
-        </thead>
-        <tbody>
-            @foreach ($perfiles as $perfil)
-                <tr>
-                    <td>{{ $perfil->idPerfil }}</td>
-                    <td>{{ $perfil->Perfil_descripcion }}</td>
-                    <td>
-                        <a href="/gestion/perfiles/{{$perfil->idPerfil}}/editar" class="btn btn-primary">Editar</a>
-                    </td>
-                </tr>
-            @endforeach
-        </tbody>
-    </table>
+    <div class="accordion" id="accordionExample">
+    @foreach($perfiles as $p)
+        
+        <div class="accordion-item">
+            <h2 class="accordion-header">
+                <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapse{{$p->idPerfil}}" aria-expanded="true" aria-controls="collapse{{$p->idPerfil}}">
+                {{$p->Perfil_descripcion}}
+                </button>
+            </h2>
+        <div id="collapse{{$p->idPerfil}}" class="accordion-collapse collapse show" data-bs-parent="#accordionExample">
+            <div class="accordion-body">
+                Modulos:
+                <ul>
+                @foreach($p->perfilmodulo as $pm)
+                    <li>{{$pm->modulo->Modulo_descripcion}}</li>
+                @endforeach
+                </ul>
+            </div>
+        </div>
+        </div>
+
+    @endforeach
+    </div>
 </div>
 @endsection
