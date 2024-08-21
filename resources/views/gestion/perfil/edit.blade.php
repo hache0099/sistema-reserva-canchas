2b040c76dffe3ca00625e3dca93c054ffb2a0235
hache0099 <hache0099_github@protonmail.com>
2024-08-20 21:55:00 -0300

    Añadidos los checkboxes para los modulos

--- resources/views/gestion/perfil/edit.blade.php
+++ resources/views/gestion/perfil/edit.blade.php
@@ -10,6 +10,13 @@
                 <input type="text" class="form-control" id="Perfil_descripcion" name="Perfil_descripcion" value="{{ $perfil->Perfil_descripcion }}" required>
             </div>
 
+            @foreach($modulo as $m)
+                <div class="form-check">
+                    <input class="form-check-input" type="checkbox" name="perfilmodulo[]" value="{{$m->idModulo}}" {{in_array($m->idModulo,$perfilmodulo) ? "checked" : ""}}>
+                    <label class="form-check-label">{{$m->Modulo_descripcion}}</label>
+                </div>
+            @endforeach
+
             <button type="submit" class="btn btn-primary">Guardar Cambios</button>
         </form>
             <a href="/gestion/perfiles" role="button" class="btn btn-danger">Cancelar</a>
