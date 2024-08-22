4cc1b3f7f0c7ba56b1719b2e1d271e990d4ae4e2
hache0099 <hache0099_github@protonmail.com>
2024-08-22 19:35:09 -0300

    Añadidas las funciones para crear una reserva y obtener las horas disponibles

--- app/Http/Controllers/ReservaController.php
+++ app/Http/Controllers/ReservaController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Reserva;
+use App\Models\Cancha;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
@@ -26,7 +27,9 @@ class ReservaController extends Controller
      */
     public function create()
     {
-        //
+        $canchas = Cancha::all();
+
+        return view('reserva.create', compact('canchas'));
     }
 
     /**
@@ -35,6 +38,21 @@ class ReservaController extends Controller
     public function store(Request $request)
     {
         //
+        $request->validate([
+            'id_cancha' => 'required|int',
+            'fecha' => 'required|date',
+            'hora' => 'required|int',
+            'id_usuario' => 'int',
+        ]);
+
+        Reserva::create([
+            'Reserva_fecha' => $request->fecha,
+            'Reserva_hora' => $request->hora,
+            'rela_usuario' => isset($request->id_usuario) ? $request->id_usuario : Auth::user()->id_usuario,
+            'rela_cancha' => $request->id_cancha,
+        ]);
+
+        return redirect()->route('reserva.index');
     }
 
     /**
@@ -67,5 +85,33 @@ class ReservaController extends Controller
     public function destroy(Reserva $reserva)
     {
         //
+    }
+
+    /**
+     * Para ser usado con métodos AJAX
+     */
+    function obtenerHorasDisponibles(Request $request)
+    {
+        $request->validate([
+            'id_cancha' => 'required|int',
+            'fecha' => 'required|date',
+        ]);
+
+        $id_cancha = $request->id_cancha;
+        $fecha = $request->fecha;
+        
+        $cancha = Cancha::find($id_cancha);
+        $canchaHorario = $cancha->canchahorario;
+
+        $horasCancha = collect([]);
+        for($hora = $canchaHorario->hora_desde; $hora <= $canchaHorario->hora_hasta; $hora++)
+        {
+            $horasCancha->push($hora);
+        }
+
+        $horasOcupadas = Reserva::where('Reserva_fecha',$fecha)->pluck('Reserva_hora');
+        $horasDisponibles = $horasCancha->diff($horasOcupadas);
+
+        return json_encode($horasDisponibles->toArray());
     }
 }
