18a67426647347be0f023bb9bd9e6b7051aa81c9
hache0099 <hache0099_github@protonmail.com>
2024-08-22 19:38:14 -0300

    Creada view para crear una nueva reserva

---
+++ resources/views/reserva/create.blade.php
@@ -0,0 +1,49 @@
+@extends('layout.mainlayout')
+@section('content')
+ <div class="container my-4">
+        <h1>Crear Nueva Reserva</h1>
+         @if ($errors->any())
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        @endif
+            
+        <form action={{route("reserva.store")}} method="post">
+            @csrf
+            
+            <!-- Selección de la cancha -->
+            <div class="form-group">
+                <label for="cancha">Cancha Elegida:</label>
+                <select class="form-control" id="cancha" name="id_cancha" required>
+                    <option value="">Seleccionar Cancha</option>
+                    @foreach($canchas as $cancha)
+                        <option value="{{ $cancha->id_cancha }}">{{$cancha->tipocancha->Material}}, {{ $cancha->Cancha_cantidad_max_personas }} personas, Precio: ${{ $cancha->Cancha_precio_hora }}</option>
+                    @endforeach
+                </select>
+            </div>
+
+            <!-- Selección de la fecha -->
+            <div class="form-group">
+                <label for="fecha">Fecha de Reserva:</label>
+                <input type="date" class="form-control" id="fecha" name="fecha" required>
+            </div>
+
+            <!-- Checkboxes de las horas disponibles -->
+            <div class="form-group">
+                <label for="horas">Horas Disponibles:</label>
+                <div id="horas-container" class="form-check">
+                    <!-- Aquí se mostrarán las horas disponibles después de la solicitud AJAX -->
+                </div>
+            </div>
+
+            <button type="submit" class="btn btn-primary">Reservar</button>
+        </form>
+    </div>
+
+@endsection
+
+@section('custom-scripts')
+<script src={{asset('js/obtenerHorasDisponibles.js')}}></script>
+@endsection
