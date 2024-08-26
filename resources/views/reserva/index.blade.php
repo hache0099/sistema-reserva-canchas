e23a12beb5f9fc72c816f7d2feb5460d2c1d9830
hache0099 <hache0099_github@protonmail.com>
2024-08-26 15:02:20 -0300

    Modificado el cómo se muestran las reservas en el index

--- resources/views/reserva/index.blade.php
+++ resources/views/reserva/index.blade.php
@@ -8,7 +8,23 @@
 			@if (count($reservas) > 0)
 				<ul>
 				@foreach($reservas as $reserva)
-					<li>{{$reserva->rela_cancha}} - {{ $reserva->Reserva_fecha }} - {{$reserva->Reserva_hora}}:00
+				<div class="row mt-3">
+					<div class="container p-3 border rounded-3">
+						<div class="d-flex w-100 justify-content-between">
+							<h4>Reserva Cod: {{$reserva->id_reserva}}</h4>
+							<h5>Fecha: {{$reserva->Reserva_fecha}}</h5>
+						</div>
+						<div class="my-3">
+							<h5>Canchas y Horas</h5>
+							Cancha: Cancha {{$reserva->cancha->id_cancha}}: {{$reserva->cancha->tipocancha->Material}}
+							Hora: {{$reserva->Reserva_hora}}:00
+							Estado: {{$reserva->reservaestado->ReservaEstado_descripcion}}
+						</div>
+						<div class="mt-3">
+							<a role="button" class="btn btn-primary" href={{route("reserva.edit",['reserva' => $reserva])}}>Editar</a>
+						</div>
+					</div>
+				</div>	
 				@endforeach
 				</ul>
 			@else
