15044c9b68fdc2531dde4b1df5cc22e8027d1c28
hache0099 <hache0099_github@protonmail.com>
2024-08-27 20:46:37 -0300

    Añadido un buscador para buscar usuarios

--- resources/views/gestion/users/index.blade.php
+++ resources/views/gestion/users/index.blade.php
@@ -2,6 +2,27 @@
 @section('content')
 <div class="container my-4">
     <h1>Lista de Usuarios y Perfiles</h1>
+    <form action="" method="get" name="buscarUsuario" id="buscarUsuario">
+        <div class="row mb-3 align-items-center">
+            <div class="col-auto">
+                <div class="form-floating">
+                    <input type="text" class="form-control" id="query" placeholder="Buscar">
+                    <label for="query" class="form-label">Buscar Usuario</label>
+                </div>
+            </div>
+            <div class="col-auto">
+                <select class="form-select">
+                    <option selected>Buscar por</option>
+                    <option value="id">ID</option>
+                    <option value="dni">DNI</option>
+                    <option value="email">Email</option>
+                    <option value="nombre">Nombre</option>
+                    <option value="apellido">Apellido</option>
+                </select>
+            </div>   
+        </div>
+
+    </form>
     <table class="table table-bordered">
         <thead>
             <tr>
