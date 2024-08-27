572b3a7cc4d5cd686a17612dca68e04fe7fc3aeb
hache0099 <hache0099_github@protonmail.com>
2024-08-27 19:50:46 -0300

    Corregido el título para que sea dinámico

--- resources/views/layout/partials/head.blade.php
+++ resources/views/layout/partials/head.blade.php
@@ -4,6 +4,6 @@
 <!--
 <meta name="author" content="">
 -->
-<title>xD</title>
+<title>@yield("title", "Documento")</title>
 <!-- Bootstrap core CSS -->
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
