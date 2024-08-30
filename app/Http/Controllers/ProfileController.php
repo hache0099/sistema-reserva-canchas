497ac936497fe67358beeabfb17cb04839f4c977
hache0099 <hache0099_github@protonmail.com>
2024-08-30 19:57:27 -0300

    Añadida las funciones para editar otros usuarios con checks de acceso según su perfil

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 use App\Models\User;
 use App\Models\Persona;
-//use App\Models\Perfil;
+use App\Models\Perfil;
 use App\Models\TipoDocumento;
 use App\Models\TipoContacto;
 use App\Models\PersonaContacto;
@@ -20,15 +20,16 @@ class ProfileController extends Controller
 {
     //
 
-    private function obtenerInfoUsuario()
+    private function obtenerInfoUsuario($id_usuario = null)
     {
-        $user = User::find(Auth::user()->id_usuario);
+        $user = User::find(isset($id_usuario) ? $id_usuario : Auth::user()->id_usuario);
         $persona = $user->persona;
         $domicilio = $persona->domicilio->Domicilio_detalle;
         $tipoDomicilio = $persona->domicilio->tipodomicilio->TipoDomicilio_desc;
         $dni = $persona->personadocumento->PersonaDocumento_desc;
         $tipodni = $persona->personadocumento->tipodocumento->TipoDocumento_desc;
         $telefono = $persona->personacontacto->first()->PersonaContacto_desc;
+        //$perfil = $user->perfil;
         
 
         return array(
@@ -39,6 +40,7 @@ class ProfileController extends Controller
             'dni' => $dni,
             'tipodni' => $tipodni,
             'telefono' => $telefono,
+            //'perfil' => $perfil,
         );
     }
 
@@ -50,7 +52,7 @@ class ProfileController extends Controller
     }
 
 
-    function update(Request $request)
+    function update(Request $request, $id=null)
     {
         $request->validate([
             'nombre' => 'required|string|max:45',
@@ -61,11 +63,20 @@ class ProfileController extends Controller
 			'domicilio' => 'required|max:100',
 			'fechanac' => 'required|date',
             'tipodomicilio' => 'numeric|min:1',
+            'id_usuario' => 'numeric',
+            'perfil' => 'numeric',
         ]);
-        
+
+        if(isset($request->editarOtroUsuario))
+        {
+            if(User::find(Auth::user()->id_usuario)->perfil->Perfil_descripcion == "Usuario")
+            {
+                return response("No Autorizado",403);
+            }
+        }
         try{
         DB::beginTransaction();
-        $user = User::find(Auth::user()->id_usuario);
+        $user = User::find(isset($id) ? $id : Auth::user()->id_usuario);
         $persona = $user->persona;
         $domicilio = $persona->domicilio;
         $personadni = $persona->personadocumento;
@@ -90,24 +101,51 @@ class ProfileController extends Controller
         $telefono->PersonaContacto_desc = $request['telefono'];
         $telefono->save();
 
+        if(isset($request->perfil))
+        {
+            $user->rela_perfil = $request->perfil;
+            $user->save();
+        }
+
         DB::commit();
 
-        return redirect('/profile')->with('status', 'Sus datos han sido modificados con éxito');
-
+        if(!isset($request->editarOtroUsuario))
+        {
+            return redirect('/profile')->with('status', 'Sus datos han sido modificados con éxito');
+        }
+        return redirect('/gestion/usuarios')->with('status', 'Los datos han sido modificados con éxito');
         } catch (Throwable $e){
             DB::rollBack();
             return back()->withErrors('Error al actualizar: ' . $e)->withInput();
         }
     }
 
-    function edit()
+    function edit($id = null)
     {
-        $infoUsuario = $this->obtenerInfoUsuario();
+        if(isset($id))
+        {
+            if(User::find(Auth::user()->id_usuario)->perfil->Perfil_descripcion == "Usuario" 
+                && $id !== Auth::user()->id_usuario)
+            {
+                return response("No Autorizado",403);
+            }
+        }
+        
+        $infoUsuario = $this->obtenerInfoUsuario($id);
         $tiposdni = TipoDocumento::all();
         $tiposdomicilio = TipoDomicilio::all();
 
         $infoUsuario['tiposdni'] = $tiposdni;
         $infoUsuario['tiposdomicilio'] = $tiposdomicilio;
+
+        $user = User::find(Auth::user()->id_usuario);
+        if($user->perfil->Perfil_descripcion != "Usuario")
+        {
+            $infoUsuario['tiposperfil'] = Perfil::all()
+                ->reject(function ($perfil) use ($user) { 
+                    return $perfil->idPerfil < $user->perfil->idPerfil; 
+                });
+        }
 
         return view('user.editprofile', $infoUsuario);
     }
