5bb19e9bf2251a460eda6b8e05f6dcc05c62ba61
hache0099 <hache0099_github@protonmail.com>
2024-09-05 21:52:53 -0300

    Creada template del correo para cambio de contraseña

---
+++ resources/views/emails/ChangePasswordEmail.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Cambio de Contraseña</title>
+</head>
+<body>
+    <h1>Bienvenido {{ $user->persona->Nombre }}</h1>
+    <p>Estimado/a {{ $user->persona->Nombre }},</p>
+    <p>Haga click <a href=localhost:8000/
+
+</body>
+</html>
