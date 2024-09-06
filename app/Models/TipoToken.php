f205b7e351a7d4d4a43c466d1e84bd869071a282
hache0099 <hache0099_github@protonmail.com>
2024-09-05 21:51:34 -0300

    Creado el modelo TipoToken que gestiona los tipos de token que pueden usarse

---
+++ app/Models/TipoToken.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class TipoToken extends Model
+{
+    use HasFactory;
+
+    protected $table = "TipoToken";
+    protected $primaryKey = "idTipoToken";
+    public $timestamps = false;
+
+    protected $fillable = [
+        'TipoToken_desc',
+    ];
+}
