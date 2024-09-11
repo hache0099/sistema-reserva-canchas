e8685efcf7e26bb0f501de42ea62bb1e5f01186e
hache0099 <hache0099_github@protonmail.com>
2024-09-11 19:21:51 -0300

    Terminada la función para modificar los permisos de acceso a un perfil

--- app/Http/Controllers/PerfilController.php
+++ app/Http/Controllers/PerfilController.php
@@ -5,6 +5,7 @@ use App\Models\Perfil;
 use App\Models\Modulo;
 use App\Models\PerfilModulo;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class PerfilController extends Controller
 {
@@ -21,7 +22,9 @@ class PerfilController extends Controller
     {
         $perfil = Perfil::find($id_perfil);
         $modulo = Modulo::all();
-        $perfilmodulo = PerfilModulo::where('Perfil_idPerfil', $perfil->idPerfil)->pluck('Modulo_idModulo')->toArray();
+        $perfilmodulo = PerfilModulo::where('Perfil_idPerfil', $perfil->idPerfil)
+            ->pluck('Modulo_idModulo')
+            ->toArray();
 
         return view('gestion.perfil.edit',compact('perfil', 'modulo','perfilmodulo'));
     }
@@ -35,9 +38,24 @@ class PerfilController extends Controller
 
         $perfil = Perfil::find($idPerfil);
 
+        try{
+        DB::beginTransaction();
         PerfilModulo::destroy($perfil->perfilmodulo->modelKeys());
 
         //TODO: terminar la actualizacion de los modulos
+        foreach($request->perfilmodulo as $idModulo)
+        {
+            PerfilModulo::create([
+                'Perfil_idPerfil' => $idPerfil,
+                'Modulo_idModulo' => $idModulo,
+            ]);
+        }
+        DB::commit();
 
+        return redirect('/gestion/perfiles')->with('status', 'se ha actualizado el perfil con éxito');
+        } catch (Throwable $e) {
+            DB::rollBack();
+            return back()->withErrors($e->getMessage());
+        }
     }
 }
