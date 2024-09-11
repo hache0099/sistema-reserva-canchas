2d9e08abe424aeeaae705444dd2d8c2c7a8dc18e
hache0099 <hache0099_github@protonmail.com>
2024-09-11 19:21:19 -0300

    Añadida la función para mostrar sólo los módulos a los que un usuario tiene autorización

--- app/Http/Controllers/GestionController.php
+++ app/Http/Controllers/GestionController.php
@@ -4,11 +4,22 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Models\Modulo;
+
+use Illuminate\Support\Facades\Auth;
+
 class GestionController extends Controller
 {
     //
     function show()
     {
-        return view("gestion.index");
+        $userPerfil = Auth::user()->perfil;
+
+        $modulos = Modulo::whereHas('perfilmodulo', function($query) use ($userPerfil) {
+            $query->where('Perfil_idPerfil', $userPerfil->idPerfil);
+        })
+        ->where('Modulo_ruta','like','/gestion/%')
+        ->get();
+        return view("gestion.index", compact('modulos'));
     }
 }
