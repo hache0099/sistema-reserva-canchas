231de952352547af0ccc67582be2001295995991
hache0099 <hache0099_github@protonmail.com>
2024-09-20 13:35:52 -0300

    Creado el modelo para la tabla EstadoPago

---
+++ app/Models/EstadoPago.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class EstadoPago extends Model
+{
+    use HasFactory;
+
+    //TODO
+
+    protected $table = "EstadoPago";
+    protected $primaryKey = "idEstadoPago";
+    public $timestamps = false;
+
+    
+    
+}
