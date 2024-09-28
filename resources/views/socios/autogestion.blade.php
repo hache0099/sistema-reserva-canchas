e7b1e946c67894914929b62fe991a791eff5b053
hache0099 <hache0099_github@protonmail.com>
2024-09-27 23:43:34 -0300

    Añadida vista para autogestión de socios

---
+++ resources/views/socios/autogestion.blade.php
@@ -0,0 +1,13 @@
+@extends('layout.mainlayout')
+@section('title', 'Autogestion de socios')
+@section('content')
+
+<div class="container my-5">
+    <!-- Header principal -->
+    <h1 class="text-center">Autogestión</h1>
+    
+    <!-- Header de Pagos -->
+    <h3 class="mt-4">Pagos</h3>
+</div>
+
+@endsection
