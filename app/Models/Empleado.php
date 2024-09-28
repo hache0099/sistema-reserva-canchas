631b052f5ef415d667e47ae82d4c859d9cec47a1
hache0099 <hache0099_github@protonmail.com>
2024-09-27 23:33:12 -0300

    Creado modelo para la tabla Empleado

---
+++ app/Models/Empleado.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Empleado extends Model
+{
+    use HasFactory;
+    protected $table = "Empleado";
+    protected $primaryKey = "idEmpleado";
+    public $timestamps = false;
+
+    protected $fillable = [
+        'codigo_legajo',
+        'fecha_alta_empleado',
+        'rela_usuario',
+    ];
+
+    function usuario()
+    {
+        return $this->belongsTo(User::class,'rela_usuario');
+    }
+}
