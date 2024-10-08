88a0f05e2b1f0716f8dbd42bbed043b0b6ef8f3c
hache0099 <hache0099_github@protonmail.com>
2024-10-08 20:07:10 -0300

    Agregados campos rellenables para el modelo IngresoEgreso

--- app/Models/IngresoEgreso.php
+++ app/Models/IngresoEgreso.php
@@ -12,6 +12,11 @@ class IngresoEgreso extends Model
     protected $primaryKey = 'idIngresoEgreso';
     public $timestamps = false;
 
+    protected $fillable = [
+        'tipo_evento',
+        'fecha_hora',
+        'rela_reserva',
+    ];
 
     function reserva()
     {
